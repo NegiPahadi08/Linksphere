8e08ca5f5f911bfa36a3f9c49f7f4a41ec2a7b5b
NegiPahadi08 <21bcs2904@cuchd.in>
2026-08-20 21:33:41 +0530

    Initial auth service setup

---
+++ src/main/java/com/linksphere/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.linksphere.auth.repository;
+
+import com.linksphere.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
