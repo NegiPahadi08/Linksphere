7700470564ba1ecd368a0f75c1e09fad18d1c648
NegiPahadi08 <21bcs2904@cuchd.in>
2026-08-21 20:24:45 +0530

    Milestone 2 Step 2.1: BCrypt password hashing implemented

--- src/main/java/com/linksphere/user/service/UserService.java
+++ src/main/java/com/linksphere/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.linksphere.user.service;
 
 import com.linksphere.user.entity.User;
 import com.linksphere.user.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -10,12 +11,15 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
